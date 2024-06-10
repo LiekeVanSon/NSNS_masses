f3f5a4bbd0390272e8de91cf8cd3b8f1ed8d8ea0
LiekeVanSon <lieke_van_son@hotmail.com>
2024-06-10 11:32:11 -0400

    finished new sets of runs

--- code/GridSubmitCOMPAS/masterfolder/MainRun/stroopwafel_interface_Lieke.py
+++ code/GridSubmitCOMPAS/masterfolder/MainRun/stroopwafel_interface_Lieke.py
@@ -32,7 +32,7 @@ compas_executable = os.path.join(os.environ.get('COMPAS_ROOT_DIR'), 'src/COMPAS'
 # 1e7 systems w. 20 cores and 2e5 systems per core takes ~7 hours for main run + 1 hour for post-processing + 10 min Cosmic integration
 # 1e7 systems w. 70 cores and 1e5 systems per core takes 1hr expl ~7 hours for sw + 1 hour AIS run + 1 hour for post-processing + 10 min Cosmic integration
 num_systems = int(1e7)              # Number of binary systems to evolve  # Note: overrides pythonSubmit value
-output_folder = '/mnt/ceph/users/lvanson/CompasOutput/v02.35.02/N1e7_CEa025_AllDCO_AIS//MainRun/'
+output_folder = '/mnt/ceph/users/lvanson/CompasOutput/v02.35.02/N1e7_beta1_AllDCO_AIS//MainRun/'
 random_seed_base = 0                # The initial random seed to increment from                                       # Note: overrides pythonSubmit value
 
 num_cores = 36                       # Number of cores to parallelize over 
@@ -267,7 +267,7 @@ if __name__ == '__main__':
     run_on_hpc = namespace.run_on_hpc #If True, it will run on a clustered system helios, rather than your pc
     mc_only = namespace.mc_only # If you dont want to do the refinement phase and just do random mc exploration
     output_filename = namespace.output_filename #The name of the output file
-    output_folder = '/mnt/ceph/users/lvanson/CompasOutput/v02.35.02/N1e7_CEa025_AllDCO_AIS//MainRun/'
+    output_folder = '/mnt/ceph/users/lvanson/CompasOutput/v02.35.02/N1e7_beta1_AllDCO_AIS//MainRun/'
 
     # Set commandOptions defaults - these are Compas option arguments
     commandOptions = dict()
