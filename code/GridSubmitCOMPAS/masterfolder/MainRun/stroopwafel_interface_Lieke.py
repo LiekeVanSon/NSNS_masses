c1dc702ccc891d29fc488b8287946aace9cd422c
LiekeVanSon <lieke_van_son@hotmail.com>
2024-07-07 12:19:41 -0400

    added load modules/2.1.1 to be compatible with older modules

--- code/GridSubmitCOMPAS/masterfolder/MainRun/stroopwafel_interface_Lieke.py
+++ code/GridSubmitCOMPAS/masterfolder/MainRun/stroopwafel_interface_Lieke.py
@@ -31,12 +31,12 @@ compas_executable = os.path.join(os.environ.get('COMPAS_ROOT_DIR'), 'src/COMPAS'
 # 1e6 sytems w. 20 cores and 1e4 systems per core takes ~1 hour)
 # 1e7 systems w. 20 cores and 2e5 systems per core takes ~7 hours for main run + 1 hour for post-processing + 10 min Cosmic integration
 # 1e7 systems w. 70 cores and 1e5 systems per core takes 1hr expl ~7 hours for sw + 1 hour AIS run + 1 hour for post-processing + 10 min Cosmic integration
-num_systems = int(1e7)              # Number of binary systems to evolve  # Note: overrides pythonSubmit value
-output_folder = '/mnt/ceph/users/lvanson/CompasOutput/v02.35.02/N1e7_beta1_AllDCO_AIS//MainRun/'
+num_systems = int(1e6)              # Number of binary systems to evolve  # Note: overrides pythonSubmit value
+output_folder = '/mnt/ceph/users/lvanson/CompasOutput/v02.46.01/N1e6_Fid_WDWDAIS//MainRun/'
 random_seed_base = 0                # The initial random seed to increment from                                       # Note: overrides pythonSubmit value
 
 num_cores = 36                       # Number of cores to parallelize over 
-num_per_core = int(1e5)              # Number of binaries per batch
+num_per_core = int(1e4)              # Number of binaries per batch
 mc_only = False                      # Exclude adaptive importance sampling (currently not implemented, leave set to True)
 run_on_hpc = True                    # Run on slurm based cluster HPC
 
@@ -45,8 +45,8 @@ debug = True                        # show COMPAS output/errors
 hdf5 = True
 
 
-### Default options for interesting systems when using AIS: ['BBH', 'DNS', 'BHNS', 'AnyDCO' ]
-sys_int = 'AnyDCO'
+### Default options for interesting systems when using AIS: ['WDWD', 'BBH', 'DNS', 'BHNS', 'AnyDCO' ]
+sys_int = 'WDWD'
 
 def create_dimensions():
     """
@@ -57,7 +57,7 @@ def create_dimensions():
     OUT:
         As Output, this should return a list containing all the instances of Dimension class.
     """
-    m1 = classes.Dimension('--initial-mass-1', 5, 150, sampler.kroupa, prior.kroupa) 
+    m1 = classes.Dimension('--initial-mass-1', 0.9, 150, sampler.kroupa, prior.kroupa) 
     q = classes.Dimension('q', 0.01, 1, sampler.uniform, prior.uniform, should_print = False)
     a = classes.Dimension('--semi-major-axis', .01, 1000, sampler.flat_in_log, prior.flat_in_log)
     return [m1, q, a]
@@ -152,6 +152,8 @@ def interesting_systems(batch):
         dco_seeds = double_compact_objects['SEED'][:]
 
         #Generally, this is the line you would want to change.
+        if sys_int == 'WDWD':
+            dco_mask = np.logical_and(np.isin(st1, [10, 11, 12]), np.isin(st2, [10, 11, 12])) #HeWD 10, COWD 11 ONeWD 12
         if sys_int == 'BBH':
             dco_mask = np.logical_and(st1 == 14, st2 == 14)
         if sys_int == 'DNS':
@@ -162,7 +164,9 @@ def interesting_systems(batch):
             dco_mask = np.logical_and(np.logical_or(st1 == 13, st1 == 14), np.logical_or(st2 == 13, st2 == 14) )
 
         merge_mask = merger_flag == 1
-        dns_mask = np.logical_and(merge_mask, dco_mask)
+        # dns_mask = np.logical_and(merge_mask, dco_mask)
+        # Lieke: for now, not focussing on merging WDWD, but any WDWD
+        dns_mask = dco_mask
 
         # Lieke: select systems of interest
         interesting_systems_seeds = set(dco_seeds[dns_mask])
@@ -267,7 +271,7 @@ if __name__ == '__main__':
     run_on_hpc = namespace.run_on_hpc #If True, it will run on a clustered system helios, rather than your pc
     mc_only = namespace.mc_only # If you dont want to do the refinement phase and just do random mc exploration
     output_filename = namespace.output_filename #The name of the output file
-    output_folder = '/mnt/ceph/users/lvanson/CompasOutput/v02.35.02/N1e7_beta1_AllDCO_AIS//MainRun/'
+    output_folder = '/mnt/ceph/users/lvanson/CompasOutput/v02.46.01/N1e6_Fid_WDWDAIS//MainRun/'
 
     # Set commandOptions defaults - these are Compas option arguments
     commandOptions = dict()
